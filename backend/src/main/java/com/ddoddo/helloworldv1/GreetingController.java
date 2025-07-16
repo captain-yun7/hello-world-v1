f6d46c2e385b7617b1413da19467d66b5c8e6682
mz\user <jslovejs182@gmail.com>
2025-07-17 08:47:10 +0900

    feature(backend) : 프론트엔드 연동으로 인한 CORS 설정 추가

--- backend/src/main/java/com/ddoddo/helloworldv1/GreetingController.java
+++ backend/src/main/java/com/ddoddo/helloworldv1/GreetingController.java
@@ -2,12 +2,14 @@ package com.ddoddo.helloworldv1;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class GreetingController {
 
     private final GreetingRepository repository;

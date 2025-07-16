394e8817766f74afdb61a4c58b2a84c39b3390d4
mz\user <jslovejs182@gmail.com>
2025-07-16 15:10:47 +0900

    Fix: @Controller to @RestController

--- src/main/java/com/ddoddo/helloworldv1/HelloController.java
+++ src/main/java/com/ddoddo/helloworldv1/HelloController.java
@@ -2,8 +2,9 @@ package com.ddoddo.helloworldv1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @GetMapping("/")
     public String hello() {
